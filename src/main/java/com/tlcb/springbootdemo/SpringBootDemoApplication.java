b1c42e0b6c72082325225f58d0dd2e517d8bd125
zhuxiaolei <524868226@qq.com>
2019-04-17 23:41:49 +0800

    springboot 官方文档学习

--- src/main/java/com/tlcb/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/tlcb/springbootdemo/SpringBootDemoApplication.java
@@ -2,13 +2,23 @@ package com.tlcb.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author zhuxiaolei
+ */
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		//SpringApplication springApplication = new SpringApplication(SpringBootDemoApplication.class);
+		//springApplication.run();
 	}
-
+    @RequestMapping("/hello1")
+    public String hello(){
+        return "Hello World";
+    }
 }
-
